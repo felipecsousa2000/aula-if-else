7010549e94a35c96da32f14da1357c681519e666
felipecsousa2000 <132520533+felipecsousa2000@users.noreply.github.com>
2025-08-25 09:38:19 -0300

    Add files via upload

---
+++ input2.py
@@ -0,0 +1,40 @@
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+# print(f"A soma é :", n1 + n2)
+
+# preco = float(input("Digite o valor da compra: "))
+# pago = float(input("Digite o valor pago: "))
+# troco = preco - pago
+# print(f"O troco será de R$",troco)
+
+##### ESTRUTURAS CONDICIONAIS
+
+# idade = 15
+# if idade >= 18:
+#     print("Você é maior de idade")
+# else:
+#     print("Você é menor de idade")
+
+# if senha == "1234":
+
+# numero = 15
+
+# if numero % 2 ==0:
+#     print("O numero par")
+# else:
+#     print("Impar")
+
+
+senha = int(input("Digite a senha: "))
+if senha == 1234:
+    print("Acesso permitido")
+else:
+    print("Acesso negado!")
+
+numero = int(input("Digite um numero inteiro:"))
+
+
+if numero % 2 ==0:
+    print("O numero é par")
+else:
+    print("O numero é impar")
